b94879fe3616d54b09b351b39f25a5eae6ecfa76
rdimaio <riccardodimaio11@gmail.com>
2025-11-03 14:02:48 +0000

    Customize template

--- src/test_policy_package/__init__.py
+++ src/test_policy_package/__init__.py
@@ -1,10 +1,10 @@
 SUPPORTED_VERSION = [">=35.0.0"] # Specify the Rucio versions supported by this policy package
 
 def get_algorithms():
-    from yourvo_rucio_policy_package.non_deterministic_pfn import YourVONonDeterministicPFNAlgorithm
-    from yourvo_rucio_policy_package.scope import YourVOScopeExtractionAlgorithm
-    from yourvo_rucio_policy_package.pfn2lfn import YourVORSEDeterministicScopeTranslation
-    from yourvo_rucio_policy_package.tape_metadata import YourVOTapeMetadataPlugin
+    from test_policy_package.non_deterministic_pfn import YourVONonDeterministicPFNAlgorithm
+    from test_policy_package.scope import YourVOScopeExtractionAlgorithm
+    from test_policy_package.pfn2lfn import YourVORSEDeterministicScopeTranslation
+    from test_policy_package.tape_metadata import YourVOTapeMetadataPlugin
     return { 
         'non_deterministic_pfn': {
             'yourvo_non_deterministic_pfn': YourVONonDeterministicPFNAlgorithm.construct_non_deterministic_pfn_yourvo
